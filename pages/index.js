2019fea867f30d86aa1d03ec45c689b3993a79a4
gilcohen67 <gil.cohen67@gmail.com>
2022-06-17 21:07:17 -0700

    working on welcome page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import styles from '../styles/Welcome.module.css';
+import styles from '../styles/Home.module.css'
 import Home from '../components/index.sample';
+import Head from 'next/head';
 
 export default function Welcome() {
   return (
-    <div>
-      <h1>Welcome to Word Chain</h1>
-      <Home />
+    <div className={styles.container}>
+      <Head>
+        <title>Word Chain MVP</title>
+
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Word Chain</h1>
+        <p className={styles.description}>{new Date().toLocaleDateString('en-US')}</p>
+        <div>
+          <div>{'Start Today\'s Challenge'}</div>
+          <button>New Game!</button>
+        </div>
+
+
+        <Home />
+      </main>
     </div>
   )
 }
