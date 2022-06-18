f8be8f611e2a584e4f7705a1899ff78d45b157bd
gilcohen67 <gil.cohen67@gmail.com>
2022-06-17 19:39:29 -0700

    initial next app commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Welcome.module.css';
+import Home from '../components/index.sample';
+
+export default function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to Word Chain</h1>
+      <Home />
+    </div>
+  )
+}
---
+++ pages/main/index.js
@@ -0,0 +1,6 @@
+
+export default function Play() {
+  return (
+    <h1>Play the game</h1>
+  )
+}
