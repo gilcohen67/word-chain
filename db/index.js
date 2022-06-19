710eae78a2c9dc86c122daf8d3012b0bc7c8aab2
gilcohen67 <gil.cohen67@gmail.com>
2022-06-19 12:33:07 -0700

    dynamo connected

--- db/index.js
+++ db/index.js
@@ -2,3 +2,23 @@ const { DynamoDB } = require('@aws-sdk/client-dynamodb');
 require('dotenv').config();
 
 console.log(process.env.AWS_DEFAULT_REGION);
+
+const client = new DynamoDB({region: process.env.AWS_DEFAULT_REGION})
+
+client.query({
+  TableName: "word-tree",
+  ExpressionAttributeValues: {
+    ':word': {
+      S: 'cent',
+    },
+  },
+  KeyConditionExpression: 'word_id = :word',
+  limit: 1
+})
+  .then(res => {
+    console.log(res)
+    console.log(res.Items[0])
+  })
+  .catch(err => {
+    console.log(err)
+  })
